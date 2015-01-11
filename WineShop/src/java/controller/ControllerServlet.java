66c2d54e0fcac483ab9c37e0aa21a0f91ede52f8
mojcak <mojca.komavec@gmail.com>
2015-01-11 11:58:12 +0100

    database connection, price result view

--- WineShop/src/java/controller/ControllerServlet.java
+++ WineShop/src/java/controller/ControllerServlet.java
@@ -7,6 +7,11 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -46,6 +51,14 @@ public class ControllerServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+    static final String DB_URL = "jdbc:mysql://mysql11.gigahost.dk:3306/goldenboy_agents";
+
+    //  Database credentials
+    static final String USER = "goldenboy";
+    static final String PASS = "1_am_g0ld3nb0y";
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -118,6 +131,79 @@ public class ControllerServlet extends HttpServlet {
         } else if (userPath.equals("/priceResults")){
             String winename = request.getParameter("wineName");
             System.out.println(winename);
+            
+            //
+            Connection conn = null;
+            Statement stmt = null;
+            try{
+               //STEP 2: Register JDBC driver
+               Class.forName("com.mysql.jdbc.Driver");
+
+               //STEP 3: Open a connection
+               System.out.println("Connecting to database...");
+               conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+               //STEP 4: Execute a query
+               System.out.println("Creating statement...");
+               stmt = conn.createStatement();
+               String sql;
+               sql = "SELECT wines.WineName, wines.WineID, agents.WineAgentID, agents.AgentRegion,\n" +
+                "wineStock.WineAgentID, wineStock.WineID, wineStock.StockPrice, wineStock.StockQuantity\n" +
+                "FROM wines, agents, wineStock\n" +
+                "WHERE wines.WineID = wineStock.WineID\n" +
+                "AND wineStock.WineAgentID = agents.WineAgentID\n" +
+                "AND wines.WineName ='"+winename+"'";
+               ResultSet rs = stmt.executeQuery(sql);
+
+               //STEP 5: Extract data from result set
+               while(rs.next()){
+                  //Retrieve by column name
+                  int wineID  = rs.getInt("WineID");
+                  String name = rs.getString("WineName");
+                  int agentID = rs.getInt("WineAgentID");
+                  int wineprice = rs.getInt("StockPrice");
+                  int quantity = rs.getInt("StockQuantity");
+                  String agentRegion = rs.getString("AgentRegion");
+                  
+                  request.setAttribute("wineID", wineID);
+                  request.setAttribute("wineAgentID", agentID);
+                  request.setAttribute("seller", agentRegion);
+                  request.setAttribute("price", wineprice);
+                  request.setAttribute("quantity", quantity);
+                  //Display values
+                  System.out.print(", name: " + name);
+               }
+               //STEP 6: Clean-up environment
+               rs.close();
+               stmt.close();
+               conn.close();
+            }catch(SQLException se){
+               //Handle errors for JDBC
+               se.printStackTrace();
+            }catch(Exception e){
+               //Handle errors for Class.forName
+               e.printStackTrace();
+            }finally{
+               //finally block used to close resources
+               try{
+                  if(stmt!=null)
+                     stmt.close();
+               }catch(SQLException se2){
+               }// nothing we can do
+               try{
+                  if(conn!=null)
+                     conn.close();
+               }catch(SQLException se){
+                  se.printStackTrace();
+               }//end finally try
+            }//end try
+            
+            
+            
+            
+            
+            
+            //
             
             request.setAttribute("winename", winename);
         }
--- WineShop/web/WEB-INF/view/priceResults.jsp
+++ WineShop/web/WEB-INF/view/priceResults.jsp
@@ -1,3 +1,5 @@
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="sql" uri="http://java.sun.com/jsp/jstl/sql"%>
 <%-- 
     Document   : priceResults
     Created on : 29-12-2014, 23:19:08
@@ -11,6 +13,8 @@
     <body>
         <%@ include file="/WEB-INF/jspf/navigation.jspf" %>
             
+    
+        
         <div id="main">
             
 
@@ -29,6 +33,11 @@
                        <div class="col-md-6"> 
                      <h2>Price results for <% String winename = (String)request.getAttribute("winename"); %><%= winename %></h2>
                    </div>
+                   
+                   <% String agentRegion = (String)request.getAttribute("seller"); %>
+                   <% int price = (Integer)request.getAttribute("price"); %>
+                   <% int quantity = (Integer)request.getAttribute("quantity"); %>
+                   
                  </div>
               
                      <div class="col-md-12"> 
@@ -47,6 +56,13 @@
                             <td>Action</td>
                         </tr>
                     </thead>
+                    <tr>
+                        <td><%= winename %></td>
+                        <td><%= agentRegion %></td>
+                        <td><%= price %>$</td>
+                        <td><input type="number" name="quantity"  min="1" max="<%= quantity %>"></td>
+                        <td>Add to basket</td>
+                    </tr>
                     
                 </table>
                       
@@ -54,5 +70,6 @@
             </div>
                </div>
         </div> <!-- ends main -->
+        
     </body>
 </html>
