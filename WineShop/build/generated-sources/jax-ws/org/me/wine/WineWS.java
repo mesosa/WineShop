2eaf0bbb83773de7fe2a27f905556f904ccec767
Mosa Al-Husseini <mosaalhusseini@gmail.com>
2015-01-12 13:25:03 +0100

    added library prolog?

--- WineShop/build/generated-sources/jax-ws/org/me/wine/WineWS.java
+++ WineShop/build/generated-sources/jax-ws/org/me/wine/WineWS.java
@@ -37,8 +37,8 @@ public interface WineWS {
      * @param sugar
      * @return
      *     returns org.me.wine.SetOfWines
-     * @throws PrologException_Exception
      * @throws Exception_Exception
+     * @throws PrologException_Exception
      */
     @WebMethod
     @WebResult(targetNamespace = "")
